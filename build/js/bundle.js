00e819bd6c834feb484ff33fc6995285798c818a
Julian Camilo Jordan Ordoñez <86069679+LianJordano@users.noreply.github.com>
2021-08-05 09:14:30 -0500

    Add files via upload

---
+++ build/js/bundle.js
@@ -0,0 +1,112 @@
+document.addEventListener("DOMContentLoaded", function(){
+    scrollNav();
+
+   /*  navegacionFija(); */
+});
+
+/* function navegacionFija() {
+
+    const barra = document.querySelector('.header');
+
+    // Registrar el Intersection Observer
+    const observer = new IntersectionObserver( function(entries) {
+        if(entries[0].isIntersecting) {
+            barra.classList.remove('fijo');
+        } else {
+            barra.classList.add('fijo');
+        }
+    });
+
+    // Elemento a observar
+    observer.observe(document.querySelector('.sobre-festival'));
+} */
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+    enlaces.forEach( function(enlace){
+         enlace.addEventListener("click", function(e){
+                e.preventDefault();
+
+                const seccion = document.querySelector(e.target.attributes.href.value);
+
+                seccion.scrollIntoView({
+                    behavior:"smooth",
+                });
+
+         });
+    })
+};
+document.addEventListener("DOMContentLoaded",function(){
+    crearGaleria();
+});
+
+function crearGaleria(){
+    const galeria = document.querySelector(".galeria-imagenes");
+
+    for (let i = 1; i <=12; i++) {
+         
+        const imagen = document.createElement("IMG");
+        imagen.src = ` build/img/thumb/${i}.webp`;
+        imagen.dataset.imagenId = i;
+
+        //Añadir funcion imagen
+        imagen.onclick = mostrarImagen ;
+
+        const lista = document.createElement("LI");
+        lista.appendChild(imagen);
+
+        galeria.appendChild(lista);
+        
+    }
+
+}
+
+function mostrarImagen(e){
+    
+    const id = parseInt(e.target.dataset.imagenId)
+
+    //Generar imagen
+    const imagen = document.createElement("IMG");
+    imagen.src = `build/img/grande/${id}.webp`;
+
+
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen)
+    overlay.classList.add("overlay")
+
+
+    //Boton para cerrar la imagen
+
+    const cerrarImagen = document.createElement("p");
+    cerrarImagen.textContent = "X";
+    cerrarImagen.classList.add("btn-cerrar");
+
+    overlay.appendChild(cerrarImagen);
+
+
+    //cuando se presiona se cierra la imagen
+
+    cerrarImagen.onclick = function(){
+        overlay.remove();
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body")
+    }
+
+    //cuando se da click cierra la imagen
+
+    overlay.onclick = function(){
+        overlay.remove();
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body")
+    }
+
+
+    //Mostrar en el html
+
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body");
+
+}
+
